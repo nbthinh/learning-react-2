329a21d076c9943e82ec46ff2ffee5dfd8d5e89f
nbthinh <nguyenbuuthinh036@gmail.com>
2024-05-01 21:05:21 +0700

    #21.2 - 2

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -9,10 +9,27 @@ class ListToDo extends React.Component {
             {id: 'todo1', title: 'Doing homework'},
             {id: 'todo2', title: 'Making videos'},
             {id: 'todo3', title: 'Fixing bugs'},
-        ]
+        ],
+        editTodo: {}
     }
-    handleEditTodo = (item) => {
-        
+    handleEditTodo = (todo) => {
+        let {listTodos, editTodo} = this.state;
+        let isEmptyObj = Object.keys(editTodo).length === 0
+        if (!isEmptyObj && editTodo.id === todo.id) {
+            let listTodosCopy = [...listTodos];
+            let currIndex = listTodosCopy.findIndex((item) => item.id === todo.id);
+            listTodosCopy[currIndex].title = editTodo.title;
+            this.setState({
+                listTodos: listTodosCopy,
+                editTodo: {}
+            })
+            toast.success("Update todo success");
+        }
+        else {
+            this.setState({
+                editTodo: todo
+            })
+        }
     }
     handleAddNewTodo = (todo) => {
         this.setState({
@@ -20,10 +37,24 @@ class ListToDo extends React.Component {
         })
         toast.success("Wow so easy!");
     }
+    handleDeleteTodo = (todo) => {
+        this.setState({
+            listTodos: this.state.listTodos.filter((item) => item !== todo)
+        })
+        toast.success('Delete success!');
+    }
+    handleOnchangeEditTodo = (event) => {
+        let editTodoCopy = {...this.state.editTodo};
+        editTodoCopy.title = event.target.value;
+        console.log("editTodoCopy = ", editTodoCopy);
+        this.setState({
+            editTodo: editTodoCopy
+        })
+    }
 
     render() {
-        let { listTodos } = this.state;
-        console.log("listTodos = ", listTodos);
+        let { listTodos, editTodo } = this.state;
+        let isEmptyObj = Object.keys(editTodo).length === 0
         return (
             <div className="list-todo-container">
                 <AddTodo
@@ -34,13 +65,31 @@ class ListToDo extends React.Component {
                         listTodos.map((item, index) => {
                             return (
                                 <div className="todo-child" key={item.id}>
-                                    <span> {index + 1} - {item.title} </span>
+                                    {
+                                        isEmptyObj === true ? 
+                                        <span> {index + 1} - {item.title} </span>
+                                        :
+                                        <>
+                                            {
+                                                item.id === editTodo.id ?
+                                                <span>{index + 1} - <input
+                                                    type="text" value={editTodo.title}
+                                                    onChange={(event) => this.handleOnchangeEditTodo(event)}
+                                                /></span>
+                                                :
+                                                <span> {index + 1} - {item.title} </span>
+                                            }
+                                        </>
+                                    }
                                     <button
                                         className="edit"
                                         onClick={() => this.handleEditTodo(item)}
-                                    >Edit</button>
+                                    >
+                                        {!isEmptyObj && item.id === editTodo.id ? "Save" : "Edit"} 
+                                    </button>
                                     <button
                                         className="delete"
+                                        onClick={() => this.handleDeleteTodo(item)}
                                     >Delete</button>
                                 </div>
                             );
